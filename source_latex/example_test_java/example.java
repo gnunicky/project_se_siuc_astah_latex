21d6c73603afda008c824adb4eda56f79fa1a7b7
gnunicky <poetrynick@gmail.com>
2015-03-08 22:41:40 +0100

    iterazione 1 progettazione modifiche

--- source_latex/example_test_java/example.java
+++ source_latex/example_test_java/example.java
@@ -1,26 +1,19 @@
-public class HelloWorld {
+public class CommandParserTest {
+ 
+   public void testGetCommand1() {
+        System.out.println("getCommand1");
+        CommandParser instance=new CommandParser("join '#Medical'"); 
+        String result = instance.getCommand();
+        assertNull(result);
+    } 
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
+    public void testGetParameter2() {
+        System.out.println("getParameter2");
+        CommandParser instance=new CommandParser("/join '#Medical'");
+        int index = 2;        
+        String expResult = null;
+        String result = instance.getParameter(index);
+        assertEquals(expResult, result);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
